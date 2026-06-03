9225f54dd4ab75111fc54981df1f5da6068988be
Luke Elrod <luke.elrod.career@gmail.com>
2026-06-02 23:30:36 -0700

    fix: equipped condition

--- src/main/java/com/iafenvoy/origins/data/condition/builtin/entity/EquippedItemCondition.java
+++ src/main/java/com/iafenvoy/origins/data/condition/builtin/entity/EquippedItemCondition.java
@@ -7,13 +7,14 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+
 import org.jetbrains.annotations.NotNull;
 
 public record EquippedItemCondition(EquipmentSlot equipmentSlot,
                                     ItemCondition itemCondition) implements EntityCondition {
     public static final MapCodec<EquippedItemCondition> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
             EquipmentSlot.CODEC.fieldOf("equipment_slot").forGetter(EquippedItemCondition::equipmentSlot),
-            ItemCondition.CODEC.fieldOf("item_condition").forGetter(EquippedItemCondition::itemCondition)
+            ItemCondition.optionalCodec("item_condition").forGetter(EquippedItemCondition::itemCondition)
     ).apply(i, EquippedItemCondition::new));
 
     @Override
@@ -23,6 +24,9 @@ public record EquippedItemCondition(EquipmentSlot equipmentSlot,
 
     @Override
     public boolean test(@NotNull Entity entity) {
-        return entity instanceof LivingEntity living && this.itemCondition.test(entity.level(), living.getItemBySlot(this.equipmentSlot));
+        if (!(entity instanceof LivingEntity living)) return false;
+        var stack = living.getItemBySlot(this.equipmentSlot);
+        boolean hasItem = !stack.isEmpty() && this.itemCondition.test(entity.level(), stack);
+        return hasItem;
     }
 }
