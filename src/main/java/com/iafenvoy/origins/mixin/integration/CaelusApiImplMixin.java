c851996c97b1f5b5601e6183473f7998e9fdde5c
IAFEnvoy <tanksheng545@126.com>
2026-06-15 09:55:17 +0800

    fix prevent elytra fly issue

--- src/main/java/com/iafenvoy/origins/mixin/integration/CaelusApiImplMixin.java
+++ src/main/java/com/iafenvoy/origins/mixin/integration/CaelusApiImplMixin.java
@@ -2,6 +2,7 @@ package com.iafenvoy.origins.mixin.integration;
 
 import com.iafenvoy.origins.attachment.OriginDataHolder;
 import com.iafenvoy.origins.data.power.builtin.prevent.PreventElytraFlightPower;
+import com.illusivesoulworks.caelus.api.CaelusApi;
 import com.illusivesoulworks.caelus.common.CaelusApiImpl;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,10 +13,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(CaelusApiImpl.class)
 public class CaelusApiImplMixin {
     @Inject(method = "canFallFly(Lnet/minecraft/world/entity/LivingEntity;)Lcom/illusivesoulworks/caelus/api/CaelusApi$TriState;", at = @At("HEAD"), cancellable = true, remap = false)
-    public void elytraFlightHook(LivingEntity livingEntity, CallbackInfoReturnable<Boolean> cir) {
+    public void elytraFlightHook(LivingEntity livingEntity, CallbackInfoReturnable<CaelusApi.TriState> cir) {
         OriginDataHolder.get(livingEntity).streamActivePowers(PreventElytraFlightPower.class).forEach(x -> {
             x.getEntityAction().execute(livingEntity);
-            cir.setReturnValue(false);
+            cir.setReturnValue(CaelusApi.TriState.DENY);
         });
     }
 }
